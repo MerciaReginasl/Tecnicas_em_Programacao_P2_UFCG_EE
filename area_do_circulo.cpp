ab998e5104444cc84317c5ab64d3c56119ce120d
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-07-12 09:00:20 -0300

    Calculando a área de um circulo

---
+++ area_do_circulo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define PI 3.1415
+/*Calcular área do Circulo*/
+using namespace std;
+
+int main() {
+  float raio, area_do_circulo;
+  cout<<"Informe o raio do circulo: "; //python print
+  cin >> raio; //python input
+  area_do_circulo = PI * raio * raio; //Operação aritmética
+  cout << "A área do circulo é: " << area_do_circulo << "m²" << endl;
+  return 0; //O programa executou com sucesso!
+  
+}
